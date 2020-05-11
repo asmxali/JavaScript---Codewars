e08387622eb3051c63e7631fd451298bd33f1f8a
Asma Ali <amino.asmaa.ali@gmail.com>
2020-05-10 21:44:10 -0400

    created a function to determin member status at a croquet club

---
+++ categorize_new_member.js
@@ -0,0 +1,29 @@
+// MY SOLUTION
+
+function openOrSenior(data) {
+  // ...
+  // create an empty variable to store results
+  let memberStatus = [];
+
+  //loop through the data to get each individual array
+  for (let memberData of data) {
+    let age = memberData[0];
+    let handicap = memberData[1];
+
+    //check seniority
+    if (age >= 55 && handicap > 7) {
+      memberStatus.push("Senior");
+    } else {
+      memberStatus.push("Open");
+    }
+  }
+  return memberStatus;
+}
+
+// BETTER SOLUTION
+
+function openOrSenior(data) {
+  return data.map(([age, handicap]) =>
+    age > 54 && handicap > 7 ? "Senior" : "Open"
+  );
+}

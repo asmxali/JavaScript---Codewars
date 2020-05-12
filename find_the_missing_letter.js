cd93268678a63759d6c7e2d66ba6428d6ce76a4f
Asma Ali <amino.asmaa.ali@gmail.com>
2020-05-12 18:10:54 -0400

    added a better solution to creating a function that filters through a list of people and returns my friends only

--- find_the_missing_letter.js
+++ find_the_missing_letter.js
@@ -20,3 +20,12 @@ function findMissingLetter(array) {
 }
 
 // BETTER SOLUTION
+function findMissingLetter(array) {
+  let first = array[0].charCodeAt(0);
+  for (let i = 1; i < array.length; i++) {
+    if (first + i !== array[i].charCodeAt(0)) {
+      return String.fromCharCode(first + i);
+    }
+  }
+  throw new Error("Invalid input");
+}

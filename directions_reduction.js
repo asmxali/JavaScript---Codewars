9ea8f286ac9309703974ec2bfc32f87807178060
Asma Ali <amino.asmaa.ali@gmail.com>
2020-05-15 17:48:02 -0400

    added a better solution to creating a function that reduces unnecessary steps in directions

--- directions_reduction.js
+++ directions_reduction.js
@@ -25,3 +25,19 @@ function dirReduc(arr) {
   }
   return arr;
 }
+
+// BETTER SOLUTION
+
+function dirReduc(plan) {
+  var opposite = {
+    NORTH: "SOUTH",
+    EAST: "WEST",
+    SOUTH: "NORTH",
+    WEST: "EAST",
+  };
+  return plan.reduce(function (dirs, dir) {
+    if (dirs[dirs.length - 1] === opposite[dir]) dirs.pop();
+    else dirs.push(dir);
+    return dirs;
+  }, []);
+}
